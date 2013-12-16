56c994f7828bef0355e0ba2d5eb707f1d9772176
Samuli Seppänen <samuli@openvpn.net>
2013-12-16 14:32:50 +0200

    Initial import to GitHub
    Signed-off-by: Samuli Seppänen <samuli@openvpn.net>

---
+++ manifests/init.pp
@@ -0,0 +1,36 @@
+#
+# == Class: checkrestart
+#
+# Install a tool for checking if any of the running processes are using files 
+# from packages that have been upgraded.
+#
+# Use checkrestart::cron to automate the checks and for emailing the results.
+#
+# == Parameters
+#
+# None at the moment
+#
+# == Examples
+#
+# include checkrestart
+#
+# == Authors
+#
+# Samuli Seppänen <samuli@openvpn.net>
+#
+# == License
+#
+# BSD-lisence
+# See file LICENSE for details
+#
+class checkrestart {
+
+# Rationale for this is explained in init.pp of the sshd module
+if hiera('manage_checkrestart', 'true') != 'false' {
+
+    if $::osfamily == 'Debian' {
+        include debiangoodies::install
+    }
+
+}
+}
